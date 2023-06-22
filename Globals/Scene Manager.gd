48f1c37d7dfe129f2ab06d0b0104af688845529e
mikelee53 <58196525+mikelee53@users.noreply.github.com>
2023-06-21 21:27:39 -0700

    added basic level manager

--- Globals/Scene Manager.gd
+++ Globals/Scene Manager.gd
@@ -9,7 +9,10 @@ func _ready():
 	GameEventBus.connect("level_completed", start_level_transition)
 	
 func start_level_transition():
-	animPlayer.play("Level Completed")
+	if (level_list[index]):
+		animPlayer.play("Level Completed")
+	else:
+		print('cant load next level')
 
 func load_next_level():
 	get_tree().change_scene_to_packed(level_list[index])
@@ -18,3 +21,7 @@ func load_next_level():
 func finish_level_transition():
 	animPlayer.play_backwards("Level Completed")
 	
+
+func _process(delta):
+	if Input.is_action_just_pressed("Interact"):
+		GameEventBus.emit_signal("level_completed")
